345d5ca0e11699cf78db2b31338e901c002b80dc
velmurugansk <velmurugan.k@zedbee.in>
2023-04-19 21:16:35 +0530

    movie list api

--- src/components/Listmovies.jsx
+++ src/components/Listmovies.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Listmovies = () => {
+  const [movielist, setMovielist] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchmovieData = () => {
+    axios("http://www.omdbapi.com/?i=tt3896198&apikey=ecba262b")
+      .then(response => {
+        setMovielist(response.data);
+        setError(null);
+      }).catch(setError);
+  }
+
+  useEffect(() => {
+    fetchmovieData()
+  }, [])
+
+  if (error) return <p>An error occurred</p>
+  
   return (
-    <div className='bg-gray-700 text-white h-screen px-20 py-10'>Listmovies</div>
-  ) 
+    <div className='bg-gray-700 text-white h-screen px-20 py-10'>
+      <ul>
+        <li key={movielist.title}>
+            <img src={movielist.Poster} alt="movieposter" />
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Listmovies
